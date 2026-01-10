dc6655338091deb7611ff3cf5427954969cb39c9
Jacob <104500838+JustExisting1@users.noreply.github.com>
2026-01-10 14:10:45 +0000

    Fixed name on loot table

--- src/main/java/com/justexisting1/fishanomics/init/FishanomicLootTables.java
+++ src/main/java/com/justexisting1/fishanomics/init/FishanomicLootTables.java
@@ -14,10 +14,10 @@ import net.neoforged.neoforge.event.LootTableLoadEvent;
 public class FishanomicLootTables {
 
     public static final ResourceKey<LootTable> FISH = register("fishing/fish");
+    public static final ResourceKey<LootTable> RESOURCE_BLOCKS = register("fishing/resource_blocks");
     public static final ResourceKey<LootTable> WOODEN_ROD = register("fishing/tiers/wooden_rod");
     public static final ResourceKey<LootTable> STONE_ROD = register("fishing/tiers/stone_rod");
     public static final ResourceKey<LootTable> IRON_ROD = register("fishing/tiers/iron_rod");
-
 
     private static ResourceKey<LootTable> register(String path){
         return BuiltInLootTables.register(ResourceKey.create(Registries.LOOT_TABLE, ResourceLocation.fromNamespaceAndPath(Fishanomics.MOD_ID, path)));
